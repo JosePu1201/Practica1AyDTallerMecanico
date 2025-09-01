d3ad8ef21bd32a6aa7e17be6b923106769f1d3f8
Rudy Reyes <rudyreyes202031213@cunoc.edu.gt>
2025-08-31 20:04:07 -0600

    add: reports backend module

---
+++ backend/Ruters/report.router.js
@@ -0,0 +1,33 @@
+const reportController = require('../Controller/reportController');
+const express = require('express');
+const router = express.Router();
+
+/*
+Reportes Operativos del Taller
+*/
+router.get('/trabajos_por_periodo', reportController.getWorksByPeriod);
+router.get('/historial_mantenimiento/:vehicleId', reportController.getMaintenanceHistoryByVehicle);
+router.get('/trabajos_completados', reportController.getCompletedWorks);
+router.get('/vehiculos', reportController.getVehicles);
+
+/*
+Reportes Financieros y de Facturación
+*/
+router.get('/ingresos_gastos', reportController.getIncomeExpensesByPeriod);
+router.get('/gastos_proveedor', reportController.getProviderExpenses);
+router.get('/proveedores', reportController.getProviders);
+
+/*
+Reportes de Inventario y Repuestos
+*/
+
+router.get('/partes_usadas', reportController.getPartUsageByPeriod);
+router.get('/partes_mas_usadas', reportController.getMostUsedPartsByVehicleType);
+
+/*
+Reportes de Clientes y Atención
+*/
+router.get('/cliente_historial/:clientId', reportController.getServiceHistoryByClient);
+router.get('/cliente_calificaciones', reportController.getServiceRatings);
+
+module.exports = router;
